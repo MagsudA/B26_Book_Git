e0d1fa276f567d3df4237cab22ec625c90bc7fc0
MagsudA <maqsud@live.com>
2022-03-29 22:01:08 -0400

    renamed pack feature_1

--- src/feature20/feature20.java
+++
@@ -1,7 +0,0 @@
-package feature20;
-
-public class feature20 {
-    public static void main(String[] args) {
-        //feature-20
-    }
-}
--- src/feature_1/main.java
+++ src/feature_1/main.java
@@ -2,5 +2,5 @@ package feature_1;
 
 public class main {
 
-    /// i created one branch feature-1
+    /// i created one branch feature-1 new
 }
