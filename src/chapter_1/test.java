13a27af7573867bb431bae9c88bb357ec297caa9
MagsudA <98862943+MagsudA@users.noreply.github.com>
2022-04-02 16:42:34 -0400

    Update test.java

--- src/chapter_1/test.java
+++ src/chapter_1/test.java
@@ -4,11 +4,8 @@ public class test {
     public static void main(String[] args) {
 
         // Regression Test
-        // Regression Test2
-
 
         // Smoke Test
-        // Smoke Test2
 
     }
 }
